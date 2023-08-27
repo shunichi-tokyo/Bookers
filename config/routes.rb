8fb75d804a88feeb711c266e0dc7635203a8824d
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-08-27 14:51:38 +0000

    [Add]TOPページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'books' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
